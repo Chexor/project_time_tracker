9a450dea4bde3af8d2d58b4213e5dbfbb13b7f1a
Tim K. <Tim.kindt@hotmail.com>
2025-11-26 00:45:10 +0100

    Refactor get_active_session method and add worksession management functions

--- db/database.py
+++ db/database.py
@@ -1,7 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 25 22:42:28 2025
+# db/database.py
 
-@author: Chex
-"""
+import sqlite3
+
+class Database:
+    def __init__(self, db_name="project_time_tracker.db"):
+        self.connection = sqlite3.connect(db_name)
+        self.cursor = self.connection.cursor()
+        self.create_tables()
+
+    def create_tables(self):
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS projects (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                is_active BOOLEAN NOT NULL CHECK (is_active IN (0, 1))
+            )
+        ''')
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS worksessions (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                project_id INTEGER NOT NULL,
+                start_time TEXT NOT NULL,
+                end_time TEXT,
+                description TEXT,
+                FOREIGN KEY (project_id) REFERENCES projects (id)
+            )
+        ''')
+        self.connection.commit()
+
+    def execute(self, query, params=()):
+        self.cursor.execute(query, params)
+        self.connection.commit()
+        return self.cursor
+
+    def close(self):
+        self.connection.close()
+
+
 
